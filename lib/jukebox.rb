761995ae46b82cfe4cdebe7bbdf044bed9b9a19f
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-18 03:37:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,3 +6,11 @@ def list
   
 def exit 
   
+  
+  #it "lists out the possible commands" do
+        expect( $stdout ).to receive(:puts).with(/I accept the following commands:/)
+        expect( $stdout ).to receive(:puts).with(/- help : displays this help message/)
+        expect( $stdout ).to receive(:puts).with(/- list : displays a list of songs you can play/)
+        expect( $stdout ).to receive(:puts).with(/- play : lets you choose a song to play/)
+        expect( $stdout ).to receive(:puts).with(/- exit : exits this program/)
+        help
