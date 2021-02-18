20782a9401d7e3eb56ffc767e0f274e8febb0298
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-18 03:43:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,13 @@ end
 def list 
  puts "Please enter a song name or number:" 
  user_input = gets.chomp
- 
+ if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs[input.to_i - 1]}"
+    elsif songs.include?(input)
+    puts "Playing #{input}"
+  else 
+    puts "Invalid input, please try again"
+  end 
 end
 
 def exit_jukebox
