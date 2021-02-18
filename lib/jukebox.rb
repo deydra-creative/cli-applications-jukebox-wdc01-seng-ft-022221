df0ed7995f7760ab78c3113c1b354ddd142c9915
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-18 03:54:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,11 @@
 
-def help 
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "exit : exits this program"
-end 
+def help
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
   
 def play (songs)
 puts "Please enter a song name or number:"
